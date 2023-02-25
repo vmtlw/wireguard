63e16bdaba49aa2ede201dc5dad9af8730dbb88a
vmtlw <63434489+vmtlw@users.noreply.github.com>
2023-02-25 16:29:51 +0300

    новый файл:    prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+#магия, выпоняется команда через раз, возможно мешают '+' в сгенерированном ключе
+for i in {1..1000}; do 
+  sed -i -r "s/^PrivateKey =.*/PrivateKey = "$(wg genkey)"/" ./wg.conf 2> /dev/null
+  if [[ $? == 0 ]]; then
+  echo succsess
+    break
+  fi
+done
