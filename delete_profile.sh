cec344d5feeb194a1c2c05d025ff65bb412acddb
Vladimir Konyshev <kv@calculate.ru>
2024-11-16 14:30:21 +0300

    Update delete_profile.sh

--- delete_profile.sh
+++ delete_profile.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
-NAME_PROFILE=$1
-IP_PROFILE=$(awk "{if(\$1== \"Address\"){print \$3}}" ./clients/$NAME_PROFILE.conf | cut -d/ -f1)
-if [[ -n $IP_PROFILE ]]; then 
-  echo удаляю из wg0 peer $IP_PROFILE
-else
-  echo что то пошло не так 
+set -euo pipefail
+IFS=$'\n\t'
+
+# Проверка аргументов
+if [[ $# -lt 1 ]]; then
+  echo "Usage: $0 <profile_name>"
   exit 1
 fi
-# закидываем список подлежащих удалению строки в массив
-number_array=( $(cat -n wg0.conf | grep -B2 $IP_PROFILE | awk '{print $1}') )
-#извлекаем последний , он же и наибольший элемент в массиве
-max=${number_array[2]}
-#извлекаем первый , он же и наименьший элемент в массиве
-min=${number_array[0]}
-#полчаем список строк в уменьшающемся порядке от (( наибольший + 1)) до (( наименьший -1 ))
-for (( i = $max+1; i > $min-1; i-- )) ; do
-  #удаляляем полученные в списке строки
-  sed -i "${i}d" wg0.conf
-  #сохраняем статус последней итерации скрипта
-  STATUS=$?
-done
-if [[ $STATUS == 0 ]]; then
-  # при успешном выполнении посленей команды удаляем профиль 
-  rm ./clients/$NAME_PROFILE.conf
- else 
-  echo нечего удалять
+
+NAME_PROFILE=$1
+CLIENT_CONFIG="./clients/$NAME_PROFILE.conf"
+WG_CONFIG="./wg0.conf"
+
+# Проверка существования конфигурационного файла клиента
+if [[ ! -f $CLIENT_CONFIG ]]; then
+  echo "Error: Client configuration file ($CLIENT_CONFIG) not found."
+  exit 1
 fi
-# Wireguard перечитывает обновленый конфиг wg0.conf
+
+# Получение IP клиента
+IP_PROFILE=$(awk '/^Address/ {print $3}' "$CLIENT_CONFIG" | cut -d/ -f1)
+if [[ -z $IP_PROFILE ]]; then
+  echo "Error: Failed to retrieve IP address from $CLIENT_CONFIG."
+  exit 1
+fi
+
+echo "Удаляю peer с IP: $IP_PROFILE из $WG_CONFIG"
+
+# Поиск строк для удаления
+LINE_NUMBERS=($(grep -B2 -n "AllowedIPs = $IP_PROFILE" "$WG_CONFIG" | cut -d: -f1))
+if [[ ${#LINE_NUMBERS[@]} -eq 0 ]]; then
+  echo "Error: Peer с IP $IP_PROFILE не найден в $WG_CONFIG."
+  exit 1
+fi
+
+# Удаление строк в обратном порядке
+for (( i=${#LINE_NUMBERS[@]}-1; i>=0; i-- )); do
+  sed -i "${LINE_NUMBERS[$i]}d" "$WG_CONFIG"
+done
+
+# Удаление конфигурационного файла клиента
+rm -f "$CLIENT_CONFIG"
+echo "Конфигурация клиента $NAME_PROFILE удалена."
+
+# Перезагрузка конфигурации WireGuard
 wg syncconf wg0 <(wg-quick strip wg0)
+echo "WireGuard конфигурация обновлена."
